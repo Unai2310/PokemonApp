e5f74b76410f2d8e4b2f8618b104a7cd5864591d
juanjorollan <unaiyanton@gmail.com>
2022-02-16 12:27:55 +0100

    HECHO

--- app/src/main/java/es/android/pokemon/ItemDetailFragment.java
+++ app/src/main/java/es/android/pokemon/ItemDetailFragment.java
@@ -4,10 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 
 import es.android.pokemon.databinding.FragmentItemDetailBinding;
@@ -52,6 +54,12 @@ public class ItemDetailFragment extends Fragment {
             if (mToolbarLayout != null) {
                 mToolbarLayout.setTitle(mItemName);
             }
+            ImageView itemImage = binding.itemImage;
+            String pokemonImageUrl =
+                    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + mItemId + ".png";
+            Glide.with(this)
+                    .load(pokemonImageUrl)
+                    .into(itemImage);
         }
     }
 }
--- app/src/main/java/es/android/pokemon/PokemonAPIService.java
+++ app/src/main/java/es/android/pokemon/PokemonAPIService.java
@@ -4,6 +4,6 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface PokemonAPIService {
-    @GET("pokemon/?limit=50")
+    @GET("pokemon/?limit=800")
     Call<PokemonFetchResults> getPokemons();
 }
